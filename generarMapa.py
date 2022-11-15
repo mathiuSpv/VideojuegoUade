9ec82051dffee26c3ae41e38d555954b3c27984d
mathiuS <mathius998@gmail.com>
2022-11-15 19:46:47 -0300

    commit

--- generarMapa.py
+++ generarMapa.py
@@ -1,8 +1,9 @@
 import random
 
-"""UTILIZAR SOLO EN EL MAPA QUE VE EL JUGADOR:"""
-NADA= -1
+ASCIILETRA= 65
+
 """UTILIZAR PARA EL MAPA GENERADOR"""
+INVISIBLE= -1
 MURALLA= 0
 VACIO= 1
 PERSONAJE= 2
@@ -32,7 +33,7 @@ def _longitudBloque(bloque: list):
 
 def genMapa(filas= 18, columnas= 32):
     """Funcion que permite crear la room del juego, usa un diccionario como un mapa y su Key es 'Key[0:1]' su fila y 'Key[1:3]' su columna respectiva"""
-    filas= (filas if filas==18 else 18) + 65
+    filas= (filas if filas==18 else 18) + ASCIILETRA
     columnas= (columnas if columnas==32 else 32)
     MAPA= dict()
     _crearMapa(MAPA, filas, columnas, MURALLA)
@@ -40,7 +41,8 @@ def genMapa(filas= 18, columnas= 32):
     return MAPA    
 
 def _crearMapa(mapa: dict, filas: int, columnas: int, rellenar: int):
-    numId= 1; letraId= 65
+    """Crea las filas y columnas respectivas con un ID para cada bloque y un valor"""
+    numId= 1; letraId= ASCIILETRA
     while letraId <= filas:
         ID= chr(letraId)+(str(numId) if numId>9 else f"0{numId}")
         mapa[ID]= rellenar
@@ -49,6 +51,7 @@ def _crearMapa(mapa: dict, filas: int, columnas: int, rellenar: int):
             letraId+=1; numId= 1
 
 def _generadores(mapa: dict, filas: int, columnas: int):
+    """Funcion que acopla todos los generados necesarios para la creacion del mapa"""
     bloquesVacios= set()
     bloquesInflexion= _initBloque()
     _genEntradas(mapa, bloquesInflexion, filas, columnas)
@@ -71,6 +74,7 @@ def _generadores(mapa: dict, filas: int, columnas: int):
     _genCompleto(mapa, bloquesDisponibles, bloquesEvento)
         
 def _genEntradas(mapa: dict, puntosInflexion: list, filas: int, columnas: int):
+    """Genera una entrada y una salida en el dict"""
     entradaFila= chr(random.randint(66,filas-1)) #EMPEZAR DE B COMO ACCSI HASTA LA PENULTIMA LETRA COMO ACCSI
     salidaFila= chr(random.randint(66,filas-1))
     entradaId= entradaFila+'01'
@@ -83,6 +87,7 @@ def _genEntradas(mapa: dict, puntosInflexion: list, filas: int, columnas: int):
     _agregarBloque(puntosInflexion, primerSlotEntrada)
     
 def _genPuntosInflexion(puntosInflexion: list, filas: int, columnas: int):
+    """Puntos que permiten conectar el mapa de forma aleatoria y con un punto final"""
     cuadrante= 1
     vecesHecho= 1
     while vecesHecho < CANTSPAWN:
@@ -95,6 +100,7 @@ def _genPuntosInflexion(puntosInflexion: list, filas: int, columnas: int):
             cuadrante= 0
 
 def _genPunto(filas: int, columnas: int, cuadrante: int):
+    """Genera puntos de inflexion por 2 cuadrantes de forma vertical"""
     sumValor= 15* cuadrante
     maxValor= ((columnas+1)//2)+sumValor
     seleccionFila= chr(random.randint(66, filas-1))
@@ -103,6 +109,7 @@ def _genPunto(filas: int, columnas: int, cuadrante: int):
     return seleccionFila+ seleccionColumna
 
 def _genRandom(bloquesVacios: set, entrada: str, puntosInflexion: list):
+    """Ciclo recursivo que permite crear el mapa hasta llegar a la salida"""
     if puntosInflexion:
         llegada= _datoBloque(puntosInflexion)
         _genCamino(bloquesVacios, entrada, llegada)
@@ -114,42 +121,46 @@ def _genRandom(bloquesVacios: set, entrada: str, puntosInflexion: list):
         _genRandom(bloquesVacios, llegada, puntosInflexion)
 
 def _genCamino(bloquesVacios, direccionInicio: str, direccionLlegada: str):
+    """Establece los parametros para generar una distancia"""
     longitudFila, longitudColumna= _calcularCamino(direccionInicio, direccionLlegada)
     fila, columna= direccionInicio[0:1], direccionInicio[1:3]
     _genBloques(bloquesVacios, fila, columna, longitudFila, longitudColumna)
 
 def _calcularCamino(direccionInicio: str, direccionLlegada: str):
+    """Calcula la distancia ± respecto a donde se encuentra un punto con el otro"""
     filaInicio,columnaInicio= direccionInicio[0:1], direccionInicio[1:3]
     filaFinal,columnaFinal= direccionLlegada[0:1], direccionLlegada[1:3]
     longitudFila= (ord(filaFinal)- ord(filaInicio))
     longitudColumna= (int(columnaFinal)- int(columnaInicio))
     return longitudFila,longitudColumna
         
-def _genBloques(bloquesVacios: set, fila: int, columna: int, longitudFila: int, longitudColumna: int):
-    movFila, movColumna= (1 if longitudFila> 0 else -1),(1 if longitudColumna> 0 else -1)
+def _genBloques(bloquesVacios: set, fila: int, columna: int, distanciaFila: int, distanciaColumna: int):
+    """Genera de movimientos tanto de forma horizontal y vertical siempre acercandose al punto que debe conectar"""
+    movFila, movColumna= (1 if distanciaFila> 0 else -1),(1 if distanciaFila> 0 else -1)
     mov= random.choice(["fila","columna"])
-    while longitudFila != 0 or longitudColumna !=  0:
-        if mov== "fila" and longitudFila != 0:
-            if longitudFila < 5 and longitudFila > -5:
-                vecesMov= longitudFila
+    while distanciaFila != 0 or distanciaColumna !=  0:
+        if mov== "fila" and distanciaFila != 0:
+            if distanciaFila < 5 and distanciaFila > -5:
+                vecesMov= distanciaFila
             else:
                 vecesMov= random.randint(1,4)*movFila
             for i in range(0,vecesMov+movFila,movFila):
                 _fila= chr(ord(fila)+i)
                 bloquesVacios.add(_fila+ columna)
-            fila= _fila; longitudFila+= -1*vecesMov
-        elif mov == "columna" and longitudColumna != 0:
-            if longitudColumna < 5 and longitudColumna > -5:
-                vecesMov= longitudColumna
+            fila= _fila; distanciaFila+= -1*vecesMov
+        elif mov == "columna" and distanciaColumna != 0:
+            if distanciaColumna < 5 and distanciaColumna > -5:
+                vecesMov= distanciaColumna
             else:
                 vecesMov= random.randint(1,4)*movColumna
             for j in range(0,vecesMov+movColumna,movColumna):
                 _columna= (str(int(columna)+j) if int(columna)+j>9 else '0'+ str(int(columna)+j))
                 bloquesVacios.add(fila+ _columna)
-            columna= _columna; longitudColumna+= -1*vecesMov
+            columna= _columna; distanciaColumna+= -1*vecesMov
         mov= random.choice(["fila","columna"])
     
 def _genEventos(bloquesDisponibles: set, bloquesEventos: dict):
+    """Genera eventos"""
     bloquesDisponibles= list(bloquesDisponibles)
     cantEnemigos= random.randint(9,12)
     cantAliados= random.randint(2,3)
@@ -180,9 +191,14 @@ def _genEventos(bloquesDisponibles: set, bloquesEventos: dict):
         numEvento+=1
     
 def _limpiarBloqueAlrededor(bloque: str, removerCasos: set, cantMaximaCaso: int, revisarMismo: bool):
+    """Busca los arrededores para compararlo con algunos casos que se especifiquen en los parametros"""
     quitar= False; maxIguales= 0; bloquesColumna= -1
     filaBase,columnaBase= ord(bloque[0:1]), int(bloque[1:])
     while bloquesColumna != 2:
+        """El 2 es debido a su posicion maxima que recorre siendo X Y su posicion  y las demas sus alrededores
+            x-1 y-1 | x-1 y | x-1 y+1
+             x  y-1 |  X Y  |  x  y+1
+            x+1 y-1 | x+1 y | x+1 y+1   """
         try:
             cont= 1 if bloquesColumna!= 0 or revisarMismo else 2
             columnaTest= columnaBase+ bloquesColumna
@@ -206,14 +222,16 @@ def _genCompleto(mapa: dict ,bloques: set, bloquesEventos: dict):
     for cord,valor in bloquesEventos.items():
         mapa[cord]= valor
 
-def genMapaJugador():
+def genMapaInvisible():
+    """Genera mapa que ve el jugador"""
     mapaSuplente= dict()
-    filas= 65+18
+    filas= ASCIILETRA+18
     columnas= 32
-    _crearMapa(mapaSuplente, filas, columnas, NADA)
+    _crearMapa(mapaSuplente, filas, columnas, INVISIBLE)
     return mapaSuplente
 
 def reconocerEntorno(mapa: dict, mapaSuplente: dict, posPersonaje: str):
+    """Aydua a sincronizar lo que ve el jugador con lo que hay en el mapa y en el mapa Invisible"""
     posFila, posColumna= ord(posPersonaje[0:1]), int(posPersonaje[1:])
     verColumna= -1
     while verColumna != 2:
@@ -241,7 +259,7 @@ def imprimirMapa(mapa: dict):
 
 def main():
     mapa= genMapa()
-    mapaJugador= genMapaJugador()
+    mapaInvisible= genMapaInvisible()
     
     for i in mapa.keys():
         print(i, end= ' ')
